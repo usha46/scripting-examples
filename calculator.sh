87677737426fd2e589645d85aa8fc25bb4f2d3eb
Ubuntu <ubuntu@ip-172-31-9-7.ap-south-2.compute.internal>
2026-06-18 10:32:24 +0000

    Second version: addition of three numbers

--- calculator.sh
+++ calculator.sh
@@ -4,7 +4,7 @@
 
 #####################################
 #addition of two numbers 
-#version =v1
+#version =v2
 #############################33
 #!/bin/bash
 
@@ -14,6 +14,9 @@ read num1
 echo "Enter second number:"
 read num2
 
-sum=$((num1 +num2))
+echo "Enter third number"
+read num3
+
+sum=$((num1 +num2 +num3))
 
 echo "Sum is: $sum"
