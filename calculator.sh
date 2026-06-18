1fd8efd0e9b76bfe190139ec89146e92da416094
Ubuntu <ubuntu@ip-172-31-9-7.ap-south-2.compute.internal>
2026-06-18 10:25:52 +0000

    first version of addition

---
+++ calculator.sh
@@ -0,0 +1,19 @@
+##################################
+#Author :Usha
+#Date : 18-06-2026
+
+#####################################
+#addition of two numbers 
+#version =v1
+#############################33
+#!/bin/bash
+
+echo "enter first number:"
+read num1
+
+echo "Enter second number:"
+read num2
+
+sum=$((num1 +num2))
+
+echo "Sum is: $sum"
